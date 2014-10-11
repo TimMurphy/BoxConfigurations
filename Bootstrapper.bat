188d65fc88a9e96036bee5757d46a2e9103b6dd0
Tim Murphy <tim@26tp.com>
2014-10-12 07:05:46 +1100

    Added powershell configuration to Bootstrapper.bat.

--- Bootstrapper.bat
+++ Bootstrapper.bat
@@ -1,30 +1,22 @@
 @echo off
 rem Prepares this machine to run configuration scripts.
 
+echo Setting powershell execution policy to unrestricted...
+powershell -NoProfile -ExecutionPolicy unrestricted -Command "Set-ExecutionPolicy Unrestricted"
+echo Set powershell execution policy to unrestricted.
+echo.
+
 if not exist C:\ProgramData\chocolatey\nul (
-    echo.
-    echo.
-    echo Installing chocolatey.
-    echo.
-    echo.
-    powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
-    echo.
-    echo.
-    echo Finished installing chocolatey.
-    echo.
-    echo.
+
+  echo Installing chocolatey...
+  powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
+  echo Finished installing chocolatey.
+  echo.
 )
 
-echo.
-echo.
-echo Installing Boxstarter.
-echo.
-echo.
+echo Installing Boxstarter...
 choco install Boxstarter
-echo.
-echo.
 echo Finished installing Boxstarter.
-echo.
 echo.
 
 echo.
