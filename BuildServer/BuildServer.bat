94135353fe2d207032aa36f528dbe6e481f3858b
Tim Murphy <tim@26tp.com>
2014-10-25 05:46:12 +1100

    Add BuildServer scripts & configuration.

---
+++ BuildServer/BuildServer.bat
@@ -0,0 +1,3 @@
+@echo off
+
+powershell -NoProfile -ExecutionPolicy unrestricted -Command .\BuildServer.ps1
---
+++ BuildServer/BuildServer.ps1
@@ -0,0 +1,6 @@
+﻿# Full path is required when boxstarter decides to reboot machine
+$boxstarterScript = Resolve-Path "BuildServer.config"
+
+Remove-Module Boxstarter.Chocolatey -ErrorAction SilentlyContinue
+Import-Module Boxstarter.Chocolatey -ErrorAction Stop
+Install-BoxstarterPackage $boxstarterScript -ErrorAction Stop
