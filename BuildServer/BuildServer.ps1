9552d855c850c7cc6a43e446b81883daf338c43a
Tim Murphy <tim@26tp.com>
2016-02-13 09:14:03 +1100

    Remove .\BuildServer

--- BuildServer/BuildServer.ps1
+++
@@ -1,6 +0,0 @@
-﻿# Full path is required when boxstarter decides to reboot machine
-$boxstarterScript = Resolve-Path "BuildServer.config"
-
-Remove-Module Boxstarter.Chocolatey -ErrorAction SilentlyContinue
-Import-Module Boxstarter.Chocolatey -ErrorAction Stop
-Install-BoxstarterPackage $boxstarterScript -ErrorAction Stop
